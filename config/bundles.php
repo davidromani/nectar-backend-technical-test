f5b95dfa0d110a45c81888d3796d1c8a27038ab0
David Romaní <david@flux.cat>
2024-10-16 11:55:47 +0200

    add composer symfony/security dependency

--- config/bundles.php
+++ config/bundles.php
@@ -6,4 +6,5 @@ return [
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
 ];
