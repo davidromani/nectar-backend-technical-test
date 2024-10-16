46f84d68d512f18fecedebccb39c86ce97ef4f81
David Romaní <david@flux.cat>
2024-10-16 11:51:19 +0200

    add entity traits

---
+++ src/Entity/Traits/EmailTrait.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Entity\Traits;
+
+trait EmailTrait
+{
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(?string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+}
---
+++ src/Entity/Traits/TitleTrait.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Entity\Traits;
+
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+trait TitleTrait
+{
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+}
