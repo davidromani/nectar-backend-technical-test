17df50525a94074f3ce1d0042d19bb7c41e3bd81
David Romaní <david@flux.cat>
2024-10-17 12:13:30 +0200

    add missing logged username

--- src/Menu/AdminTopRightNavMenuBuilder.php
+++ src/Menu/AdminTopRightNavMenuBuilder.php
@@ -4,23 +4,25 @@ namespace App\Menu;
 
 use Knp\Menu\FactoryInterface;
 use Knp\Menu\ItemInterface;
+use Symfony\Bundle\SecurityBundle\Security;
 
 final readonly class AdminTopRightNavMenuBuilder
 {
-    public function __construct(private FactoryInterface $factory)
-    {
+    public function __construct(
+        private FactoryInterface $factory,
+        private Security $security,
+    ) {
     }
 
     public function createTopRightUserNavMenu(): ItemInterface
     {
-        $username = '';
         $menu = $this->factory->createItem('topnav');
         $menu->setChildrenAttribute('class', 'nav navbar-nav navbar-right');
         $menu
             ->addChild(
                 'username',
                 [
-                    'label' => sprintf('<i class="far fa-user fa-fw margin-r-5"></i> %s', $username),
+                    'label' => sprintf('<i class="far fa-user fa-fw margin-r-5"></i> %s', $this->security->getUser()?->getUserIdentifier()),
                     'uri' => '#',
                 ]
             )
